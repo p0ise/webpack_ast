2d28d8e8340cd58645fa39b6cdf484952526d3db
592767809 <592767809@qq.com>
2021-11-19 17:00:40 +0800

    更新

--- iqiyi/test.js
+++ iqiyi/test.js
@@ -1,26 +1,25 @@
-// window = this;
-// navigator = {
-//     appName: 'Netscape'
-// };
-// localStorage = {
-//     getItem: function () {
-//         return null;
-//     }
-// };
-// sessionStorage = {
-//     getItem: function () {
-//         return null;
-//     }
-// };
-//
-// document = {
-//     cookie: ""
-// };
+const { JSDOM } = require("jsdom");
+const dom = new JSDOM("<html><head></head><body><p>hello word</p></body></html>", {
+    url: "http://www.iqiyi.com/",
+    referrer: "http://www.iqiyi.com/",
+    contentType: "text/html",
+    includeNodeLocations: true,
+    storageQuota: 1000000
+});
+global.window = dom.window;
+global.document = window.document;
+global.self = window;
+document.domain = "iqiyi.com";
+
 
 const t = require('./webpack_out');
 
+delete process;
+delete require;
+// delete global;
+module = undefined;
 
-let dutO = t('dutO').a;
-cons
+let dutO = t('295');
+console.log(dutO.cmd5x("/dash/asd"));
 
 
